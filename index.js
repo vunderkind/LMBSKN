9cf792e331e3c38e4741ec2720658985b072585a
mogwai <feelirabs@gmail.com>
2020-08-13 17:46:05 +0100

    Added glitchboxes

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
-gsap.to('h1', {
-    duration: 1,
-    x: 200,
-    scale: 2
-  })
+let text = document.getElementById('header');
+let count = 30;
+for (let i=0; i<=count;i++){
+    let glitchBox = document.createElement('div');
+    glitchBox.className = 'box';
+    text.appendChild(glitchBox);
+}
+
+let glitch = document.getElementsByClassName('box');
+setInterval(function(){
+for (let i=0;i<=glitch.length;i++){
+    glitch[i].style.left = Math.floor(Math.random()*100) + 'vw';
+    glitch[i].style.top = Math.floor(Math.random()*100) + 'vh';
+    glitch[i].style.width = Math.floor(Math.random()*400) + 'px';
+    glitch[i].style.height = Math.floor(Math.random()*100) + 'px';
+
+}
+},200)
